51b2f1e8f848e8fec7735ff67eb69a9a115725d9
yash2174 <yashpaithane2004@gmail.com>
2025-10-25 04:16:54 -0700

    updated

--- streamify-video-calls-master/frontend/src/lib/axios.js
+++ streamify-video-calls-master/frontend/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "https://whispr-delta.vercel.app/"
-    : "https://whispr-ysdh.onrender.com"; // 👈 replace with your actual Render backend URL
+    ? "http://localhost:5001/api" // local backend
+    : "https://whispr-ysdh.onrender.com/api"; // Render backend
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
